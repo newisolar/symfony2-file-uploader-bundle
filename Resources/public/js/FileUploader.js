4a1b9297e663a74d89655cc04c31386403aabb19
Frank Weigmann <sunrise2k4@googlemail.com>
2012-11-07 10:40:59 +0100

    remove console.log

--- Resources/public/js/FileUploader.js
+++ Resources/public/js/FileUploader.js
@@ -18,7 +18,6 @@ function PunkAveFileUploader(options)
   self.addExistingFiles = function(files)
   {
     _.each(files, function(file) {
-        console.log(file);
       appendEditableImage({
         // cmsMediaUrl is a global variable set by the underscoreTemplates partial of MediaItems.html.twig
         'thumbnail_url': file.thumbnail_url,
